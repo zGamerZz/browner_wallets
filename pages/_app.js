f238561b8304a7f817b6b2c27a678a8ee4259459
zgamerzz <h.elghachtouli@gmail.com>
2025-01-23 20:26:09 +0100

    Design

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <head>
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto+Mono&display=swap" rel="stylesheet" />
+            </head>
+            <Component {...pageProps} />
+        </>
+    );
 }
